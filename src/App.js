a33064b33b147c6b369ebab735d3dc2b03e9a2a8
Bob Egan <bob.egan.dev@gmail.com>
2024-04-18 08:25:38 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You can Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
